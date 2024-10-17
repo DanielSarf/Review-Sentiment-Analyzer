ff83872ff4e77a34e82da2529a981dd51d3591e9
Daniel Sarfraz <72676273+DanielSarf@users.noreply.github.com>
2024-10-17 15:15:58 +0500

    Refactored and took screenshots of output

--- helper_functions.py
+++ helper_functions.py
@@ -1,14 +1,27 @@
 import re
 from nltk.corpus import stopwords
 import nltk
+import joblib
 
-nltk.download('stopwords')
-STOPWORDS = set(stopwords.words('english'))
+nltk.download("stopwords")
+STOPWORDS = set(stopwords.words("english"))
 
 def preprocess_text(text):
     """Clean and preprocess text data."""
     text = text.lower()
-    text = re.sub(r'<.*?>', ' ', text)  # Replace tags with space
-    text = re.sub(r'[^a-z\s]', '', text)  # Remove non-alphabetic chars
+    text = re.sub(r"<.*?>", " ", text)  # Replace tags with space
+    text = re.sub(r"[^a-z\s]", "", text)  # Remove non-alphabetic chars
     tokens = [word for word in text.split() if word not in STOPWORDS]
-    return ' '.join(tokens)
+    return " ".join(tokens)
+
+def load_model(model_path = "Checkpoints/naive_bayes_model.pkl", vectorizer_path = "Checkpoints/vectorizer.pkl"):
+    """Load the trained model and vectorizer."""
+    model = joblib.load(model_path)
+    vectorizer = joblib.load(vectorizer_path)
+    return model, vectorizer
+
+def save_model(model, vectorizer, model_path = "Checkpoints/naive_bayes_model.pkl", vectorizer_path = "Checkpoints/vectorizer.pkl"):
+    """Save the trained model and vectorizer."""
+    joblib.dump(model, model_path)
+    joblib.dump(vectorizer, vectorizer_path)
+    print(f"Model and vectorizer have been saved.")
--- run.py
+++ run.py
@@ -1,27 +1,17 @@
-import joblib
-from helper_functions import preprocess_text
+from helper_functions import preprocess_text, load_model
 
-def load_model():
-    """Load the trained model and vectorizer."""
-    model = joblib.load('Checkpoints/naive_bayes_model.pkl')
-    vectorizer = joblib.load('Checkpoints/vectorizer.pkl')
-    return model, vectorizer
+model, vectorizer = load_model()
 
-def main():
-    model, vectorizer = load_model()
-    print("Model loaded successfully!")
+print("Model loaded successfully!")
 
-    while True:
-        user_input = input("\nEnter a review (or type 'exit' to quit): ")
-        if user_input.lower() == 'exit':
-            print("Exiting the program. Goodbye!")
-            break
+while True:
+    user_input = input("\nEnter a review (or type 'exit' to quit): ")
+    if user_input.lower() == "exit":
+        print("Exiting program.")
+        break
 
-        # Preprocess and classify the review
-        cleaned_review = preprocess_text(user_input)
-        X = vectorizer.transform([cleaned_review])
-        prediction = model.predict(X)[0]
-        print(f"Predicted Class: {prediction}")
-
-if __name__ == "__main__":
-    main()
+    # Preprocess and classify the review
+    cleaned_review = preprocess_text(user_input)
+    X = vectorizer.transform([cleaned_review])
+    prediction = model.predict(X)[0]
+    print(f"Predicted Class: {prediction}")
